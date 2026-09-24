f3f4e06f2f0c881102921e81971456d4512a4db7
Frederic nga-ngong <Frederic.nga-ngong@stu.ocsb.ca>
2026-09-24 13:34:55 +0000

    calculate circumference using Tau

--- circ_circle_tau.py
+++ circ_circle_tau.py
@@ -3,11 +3,10 @@
 # Date: Feb 18 2008
 # Calculate circumference of circle
 def main():
-    # assign a value to tau
-    tau = 6.28
+    from constants import TAU
     radius = int(input("enter a variable :"))
     # calculate circumference
-    circumference = tau * radius
+    circumference = TAU * radius
     print(
         "the circumference of a circle of radius {} is {}".format(radius, circumference)
     )
